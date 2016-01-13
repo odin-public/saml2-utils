76c55e972c2cb143e2b11b39901f1658f554235c
Gleb Averchuk <gaverchuk@parallels.com>
2016-01-13 17:30:52 +0300

    using top level DOMElement class

--- src/SOUtils/XMLConverter.php
+++ src/SOUtils/XMLConverter.php
@@ -5,7 +5,7 @@ namespace SOUtils;
 class XMLConverter {
     // Converts passed string to XML element object
     // @param [string] $str with raw XML
-    // @return [DOMElement] the XML element object
+    // @return [\DOMElement] the XML element object
     public static function str_to_xml($str) {
         return dom_import_simplexml(simplexml_load_string($str));
     }
@@ -13,7 +13,7 @@ class XMLConverter {
     // Converts passed XML element object to string
     // @param $xml element object
     // @return [string] the string with xml
-    public static function xml_to_str(DOMElement $xml) {
+    public static function xml_to_str(\DOMElement $xml) {
         return $xml->ownerDocument->saveXML($xml);
     }
 }
